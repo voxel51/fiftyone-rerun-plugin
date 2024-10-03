717ed86fcd6ca4a9debcbe6c06ce003b7c5fa1f0
Sashank Aryal <sashank2012@gmail.com>
2024-10-03 16:05:06 -0500

    module vite

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import { PluginComponentType, registerComponent } from "@fiftyone/plugins";
+import React from "react";
+
+const App = () => {
+  return <div>hi</div>;
+};
+
+const RerunFileDescriptor = {
+  EMBEDDED_DOC_TYPE: "fiftyone.utils.rerun.RrdFile",
+};
+
+registerComponent({
+  name: "Rerun",
+  label: "Rerun",
+  component: App,
+  activator: (ctx) => {
+    // only activate if schema has rrd file
+    return true;
+    // return doesSchemaContainEmbeddedDocType(
+    //   ctx.schema,
+    //   RerunFileDescriptor.EMBEDDED_DOC_TYPE
+    // );
+  },
+  type: PluginComponentType.Panel,
+  panelOptions: {
+    surfaces: "modal",
+    helpMarkdown: `Rereun viewer for FiftyOne`,
+  },
+});
